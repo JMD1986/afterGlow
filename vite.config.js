8aadb0cc1484168752acc16d947dd05fbccd4757
John Daugherty <Johndaugherty1986@gmail.com>
2026-03-30 18:48:42 -0500

    updated config for render.com deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/afterGlow/",
+  preview: {
+    allowedHosts: ["afterglow-damp.onrender.com"],
+  },
   test: {
     environment: "jsdom",
     globals: true,
