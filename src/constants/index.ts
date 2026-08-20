064d8a6401ef754e9417d2176a39d3b6e816eaf4
John-Daugherty <johndaugherty1986@gmail.com>
2026-08-20 16:20:50 -0500

    updated site

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 import { afterglowIgPost } from "../assets/images";
 
 export const TICKET_URL =
-  "https://www.eventbrite.com/e/afterglow-a-music-comedy-and-arts-carnival-tickets-1995508564835?aff=oddtdtcreator&_gl=1*14vd3nf*_up*MQ..*_ga*MTQ5MzgxODczMS4xNzg1MTgwNDQ0*_ga_TQVES5V6SH*czE3ODUxODA0NDQkbzEkZzEkdDE3ODUxODA0NTMkajUxJGwwJGgw";
-
+"https://www.eventbrite.com/e/afterglow-a-music-comedy-and-arts-carnival-tickets-1995508564835?aff=oddtdtcreator&utm_source=ig&utm_medium=social&utm_content=link_in_bio"
 export const CURRENT_POSTER = afterglowIgPost;
